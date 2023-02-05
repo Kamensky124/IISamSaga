184d386632425f1a84d04ac07162cdce680572c6
Kamensky124 <kamensky24@gmail.com>
2023-02-05 23:24:10 +0700

    Add link with backend end point

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import {useState} from "react";
+
+
+//синхронизируем типизацию через выгрузку JSON в JSON2TS сервис с переименованием согласно документации API https://todolists.samuraijs.com/swagger/index.html#/TodoLists/get_api_1_0_todolists
+
+export interface PhotoSizeViewModel {
+  url: string;
+  width: number;
+  height: number;
+  fileSize: number;
+}
+
+export interface TodolistImagesViewModel {
+  main: PhotoSizeViewModel[];
+}
+
+export interface TodolistViewModel {
+  isImportant: boolean;
+  id: string;
+  title: string;
+  description: string;
+  addedDate: Date;
+  order: number;
+  images: TodolistImagesViewModel;
+}
+
+export interface GetTodolistsResponseType {
+  pagesCount: number;
+  page: number;
+  pageSize: number;
+  totalCount: number;
+  items: TodolistViewModel[];
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todolists, setTodolists] = useState<GetTodolistsResponseType | null> (null)
 
   return (
     <div className="App">
-      <div>
+      {todolists == null
+          ? 'loading'
+          : todolists.items.map(t=>{
+            return <div>{t.title}</div>
+          })
 
+
+      }
     </div>
   )
 }
 
 export default App
+
