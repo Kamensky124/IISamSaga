37017f7af3685eea485a7bb4ef0dbeb42b30d37c
Kamensky124 <kamensky24@gmail.com>
2023-02-06 11:30:31 +0700

    Add link with backend end point

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 
 //синхронизируем типизацию через выгрузку JSON в JSON2TS сервис с переименованием согласно документации API https://todolists.samuraijs.com/swagger/index.html#/TodoLists/get_api_1_0_todolists
@@ -37,15 +38,25 @@ function App() {
 
   const [todolists, setTodolists] = useState<GetTodolistsResponseType | null> (null)
 
+  useEffect (()=>{
+    axios
+        .get<GetTidilistsResponseType>('https://todolists.samuraijs.com/api/1.0/todolists?pageNumber=1&pageSize=10')
+        .then(response=>{
+          setTodolists(response.data)
+        })
+    debugger
+  }, [])
+
+
+
+
   return (
     <div className="App">
-      {todolists == null
+      {todolists ===null
           ? 'loading'
-          : todolists.items.map(t=>{
-            return <div>{t.title}</div>
+          : todolists.items.map (todolist => {
+            return <div>{todolist.title}</div>
           })
-
-
       }
     </div>
   )
